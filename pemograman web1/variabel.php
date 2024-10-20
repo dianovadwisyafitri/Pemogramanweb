d9e021bce2d5c269006ce0d7844e33d2c9b3cf5c
dianovadwisyafitri <162030627+dianovadwisyafitri@users.noreply.github.com>
2024-10-20 01:07:11 -1200

    tugas ke2

--- pemograman web1/variabel.php
+++ pemograman web1/variabel.php
@@ -4,7 +4,7 @@
 <title> contoh variabel php</title>
 </head>
 <body>
-    <h1>halo nama saya Dianova</h1>
+    <h1>halo  nama saya Cahaya kamila</h1>
     <p>umur saya 19 tahun</p>
 </body>
 </html>
